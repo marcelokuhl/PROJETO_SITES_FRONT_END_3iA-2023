d3bbc5a89da00e2d6d0997c1d3abb3f751ad4cca
MARCELO GUILHERME KÜHL <68967909+marcelokuhl@users.noreply.github.com>
2025-12-10 14:49:25 -0300

    Add files via upload

---
+++ JULIO/site/script.js
@@ -0,0 +1,24 @@
+const $input = document.getElementById('number');
+const $spans = document.querySelectorAll('span');
+
+$input.addEventListener('input', (event) => {
+    const dividend = parseInt(event.target.value);
+
+    if (isNaN(dividend)) {
+        $spans.forEach(($span) => {
+            $span.classList.remove('true');
+        });
+
+        return;
+    }
+
+    $spans.forEach(($span) => {
+        const divisor = $span.dataset.divisor;
+
+        if (dividend % divisor === 0) {
+            $span.classList.add('true');
+        } else {
+            $span.classList.remove('true');
+        }
+    });
+});
